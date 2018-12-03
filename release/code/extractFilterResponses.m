5fd333e9ae80e464d5580e47a6f58dc44d5b14a8
Rajiv Ranjan <rajivranjan@Rajivs-MacBook-Air.local>
2018-12-03 03:32:05 -0500

    initialize

---
+++ release/code/extractFilterResponses.m
@@ -0,0 +1,12 @@
+function [filterResponses] = extractFilterResponses(img, filterBank)
+% Extract filter responses for the given image.
+% Inputs: 
+%   img:                a 3-channel RGB image with width W and height H
+%   filterBank:         a cell array of N filters
+% Outputs:
+%   filterResponses:    a W x H x N*3 matrix of filter responses
+
+
+% TODO Implement your code here
+
+end
